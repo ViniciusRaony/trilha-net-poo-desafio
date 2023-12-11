fd5b99e6ad83df5972f2b62aa29ddc85e0fb8827
ViniciusRaony <viniciusraony@hotmail.com>
2023-12-11 03:55:43 -0300

    Added test cases for iPhone and Nokia smartphone instances

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone iPhone:");
+Iphone iphone1 = new Iphone(numero: "981402683", modelo: "iPhone 14 PRO", imei: "123456789", memoria: 128 );
+
+iphone1.Ligar();
+iphone1.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Iphone iphone2 = new Iphone(numero: "981302681", modelo: "iPhone 13", imei: "373456789", memoria: 64 );
+
+iphone1.Ligar();
+iphone1.InstalarAplicativo("Clash Of Clans");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia1 = new Nokia(numero: "981552682", modelo: "G21 Plus", imei: "000000000", memoria: 128 );
+
+nokia1.ReceberLigacao();
+nokia1.InstalarAplicativo("Nubank");
